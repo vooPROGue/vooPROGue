2d372f77d143a393be960329f94986c93f07dff5
vooPROGue <vooprogue@gmail.com>
2022-02-05 01:22:27 +0200

    neumorphism

--- main.js
+++ main.js
@@ -6,13 +6,50 @@
      const btnContainer =document.querySelector(".btn-container");
      const btnPortfolio = document.querySelector(".portfolio-btn");
      const btnContact = document.querySelector(".contact-btn");
+    
+    const portfolio = document.createElement("div");
+     document.body.appendChild(portfolio);
+     portfolio.classList.add("portfolio-overlay");
+
+     const contact = document.createElement("div");
+     document.body.appendChild(contact);
+     contact.classList.add("contact-overlay");
+
+     btnPortfolio.addEventListener("click", showPortfolio);
+
+
+     function showPortfolio(){
+       portfolio.classList.toggle("show-portfolio");
+       footer.classList.add("close-footer");
+     }
+     btnContact.addEventListener("click", showContact);
+     
+     function showContact(){
+      contact.classList.toggle("show-contact");
+    }
+
+    // const closePortfolio = document.createElement("div");
+    // portfolio.appendChild(closePortfolio);
+    // closePortfolio.classList.add("close-portfolio");
+
+    // const closeContact = document.createElement("div");
+    // contact.appendChild(closeContact);
+    // closeContact.classList.add("close-contact");
+
+    const glass = document.createElement("div");
+    portfolio.appendChild(glass);
+    glass.classList.add("glass");
+
+
+    
 
      menuBtn.addEventListener("click",showMenuBtns);
 
      function showMenuBtns(){
-
       btnContainer.classList.toggle("show-btns");
-      console.log("fiwehfio");
-    }
+      }
+
+      
+      
      
      }
